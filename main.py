c01f17c6cde2e24cb5a4ba80157179cb82022972
galeonaso <gabrielite666@gmail.com>
2020-02-03 00:17:08 +1300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#saludo = "Hola "
+#nombre = input ("Ingreses su nombre: ")
+
+#print(saludo + nombre)
+
+
+'''
+Comentario multiples comillas
+'''
+num1 = int(input ("Ingrese el primer numero: "))
+num2 = int(input ("Ingrese el segundo numero: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
